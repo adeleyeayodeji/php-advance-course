dc51e005201e37042b58c89e768d5e760c4d8037
Adeleye Ayodeji <biodunhi@gmail.com>
2022-11-02 23:19:08 +0100

    Added color methods

--- index.php
+++ index.php
@@ -17,17 +17,36 @@ class Fruit
     {
         return $this->name;
     }
+
+    //method to set color
+    public function set_color($color)
+    {
+        $this->color = $color;
+    }
+
+    //get color method
+    public function get_color()
+    {
+        return $this->color;
+    }
 }
 
 //define the object
 $apple = new Fruit();
 $apple->set_name("Apple");
+$apple->set_color("Blue");
 
 //banana
 $banana = new Fruit();
 $banana->set_name("Banana");
+$banana->set_color("Green");
 
 //result
 echo $banana->get_name();
 echo "<br />";
 echo $apple->get_name();
+echo "<br />";
+echo "<br />";
+echo $banana->get_color();
+echo "<br />";
+echo $apple->get_color();
