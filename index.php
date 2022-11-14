8109a8783a05bfcc187a7ce11e9bf856d7817a80
Adeleye Ayodeji <biodunhi@gmail.com>
2022-11-14 14:56:16 +0100

    Added interface

--- index.php
+++ index.php
@@ -1,83 +1,149 @@
 <?php
-//defined custom class
-abstract class Car
+//defined interface
+interface Animal
 {
-    public $name;
-    public $number;
-    public function __construct($name, $number)
-    {
-        $this->name = $name;
-        $this->number = $number;
-    }
+    public function makeSound();
 
-    //intro
-    abstract public function intro(): string;
-
-    //int data type
-    abstract public function int(): int;
+    public function eat(): string;
 }
 
-//child classes
-class Audi extends Car
+//define child class
+class Cat implements Animal
 {
-    public function intro(): string
+    public function makeSound()
     {
-        return "Choose German quality! I'm an $this->name!";
+        echo "Meow";
     }
 
-    public function int(): int
+    public function eat(): string
     {
-        return $this->number;
+        return "Cat is eating";
     }
 }
 
-//another child class
-class Volvo extends Car
+//define child class
+class Dog implements Animal
 {
-    public function intro(): string
+    public function makeSound()
     {
-        return "Proud to be Swedish! I'm a $this->name!";
+        echo "Woof";
     }
 
-    public function int(): int
+    public function eat(): string
     {
-        return $this->number;
+        return "Dog is eating";
     }
 }
 
-//another class
-class Citroen extends Car
+//define child class
+class Mouse implements Animal
 {
-    public function intro(): string
+    public function makeSound()
     {
-        return "French extravagance! I'm a $this->name!";
+        echo "Squeak";
     }
 
-    public function int(): int
+    public function eat(): string
     {
-        return $this->number;
+        return "Mouse is eating";
     }
 }
 
-//create objects from the child classes
-$audi = new Audi("Audi", 50);
-$volvo = new Volvo("Volvo", 2);
-$citroen = new Citroen("Citroen", 45);
+//make an instance of the class
+$cat = new Cat();
+$dog = new Dog();
+$mouse = new Mouse();
+//add object to an array
+$animals = array($cat, $dog, $mouse);
+//foreach loop
+foreach ($animals as $animal) {
+    echo "<br>";
+    $animal->makeSound();
+    echo "<br>";
+    echo $animal->eat();
+    echo "<br>";
+}
 
-//get the values
-echo $audi->intro();
-echo "<br>";
-echo $volvo->intro();
-echo "<br>";
-echo $citroen->intro();
-echo "<br>";
 
-//get the int values
-echo $audi->int();
-echo "<br>";
-echo $volvo->int();
-echo "<br>";
-echo $citroen->int();
+
+// abstract class Car
+// {
+//     public $name;
+//     public $number;
+//     public function __construct($name, $number)
+//     {
+//         $this->name = $name;
+//         $this->number = $number;
+//     }
+
+//     //intro
+//     abstract public function intro(): string;
+
+//     //int data type
+//     abstract public function int(): int;
+// }
+
+// //child classes
+// class Audi extends Car
+// {
+//     public function intro(): string
+//     {
+//         return "Choose German quality! I'm an $this->name!";
+//     }
+
+//     public function int(): int
+//     {
+//         return $this->number;
+//     }
+// }
+
+// //another child class
+// class Volvo extends Car
+// {
+//     public function intro(): string
+//     {
+//         return "Proud to be Swedish! I'm a $this->name!";
+//     }
+
+//     public function int(): int
+//     {
+//         return $this->number;
+//     }
+// }
+
+// //another class
+// class Citroen extends Car
+// {
+//     public function intro(): string
+//     {
+//         return "French extravagance! I'm a $this->name!";
+//     }
+
+//     public function int(): int
+//     {
+//         return $this->number;
+//     }
+// }
+
+// //create objects from the child classes
+// $audi = new Audi("Audi", 50);
+// $volvo = new Volvo("Volvo", 2);
+// $citroen = new Citroen("Citroen", 45);
+
+// //get the values
+// echo $audi->intro();
+// echo "<br>";
+// echo $volvo->intro();
+// echo "<br>";
+// echo $citroen->intro();
+// echo "<br>";
+
+// //get the int values
+// echo $audi->int();
+// echo "<br>";
+// echo $volvo->int();
+// echo "<br>";
+// echo $citroen->int();
 
 
 // class Fruit
