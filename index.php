a1c4e007ffe09749af83e1aec46d7ca072c3c41d
Adeleye Ayodeji <biodunhi@gmail.com>
2022-11-14 16:03:07 +0100

    Added static property

--- index.php
+++ index.php
@@ -1,5 +1,18 @@
 <?php
+//define static property
+class pi
+{
+    public static $value = 3.14159;
 
+    public function staticValue()
+    {
+        return self::$value;
+    }
+}
+
+// Get static property
+$pi = new pi();
+echo $pi->staticValue();
 
 //define static methods
 // class Greeting
