4a982ec2c827c5d935f30d83adbccb7476032ac7
Adeleye Ayodeji <biodunhi@gmail.com>
2022-11-14 10:52:11 +0100

    Added abstract class

--- index.php
+++ index.php
@@ -1,44 +1,131 @@
 <?php
 //defined custom class
-class Fruit
+abstract class Car
 {
-    protected $name;
-    protected $color;
-
-    public function __construct($name, $color)
+    public $name;
+    public $number;
+    public function __construct($name, $number)
     {
         $this->name = $name;
-        $this->color = $color;
+        $this->number = $number;
     }
 
-    public function intro()
+    //intro
+    abstract public function intro(): string;
+
+    //int data type
+    abstract public function int(): int;
+}
+
+//child classes
+class Audi extends Car
+{
+    public function intro(): string
     {
-        echo "The fruit sd sdsds is {$this->name} and the color is {$this->color}.";
+        return "Choose German quality! I'm an $this->name!";
+    }
+
+    public function int(): int
+    {
+        return $this->number;
     }
 }
 
-// Strawberry is inherited from Fruit
-class Strawberry extends Fruit
+//another child class
+class Volvo extends Car
 {
-    public $weight;
-
-    public function __construct($name, $color, $weight)
+    public function intro(): string
     {
-        $this->name = $name;
-        $this->color = $color;
-        $this->weight = $weight;
+        return "Proud to be Swedish! I'm a $this->name!";
     }
 
-    public function intro()
+    public function int(): int
     {
-        echo "The fruit is {$this->name}, the color is {$this->color}, and the weight is {$this->weight}";
-    }
-
-    public function addition($num1, $num2)
-    {
-        echo $num1 + $num2;
+        return $this->number;
     }
 }
-$strawberry = new Strawberry("Strawberry", "red", "30kg");
-// $strawberry->addition(5, 10);
-$strawberry->intro();
+
+//another class
+class Citroen extends Car
+{
+    public function intro(): string
+    {
+        return "French extravagance! I'm a $this->name!";
+    }
+
+    public function int(): int
+    {
+        return $this->number;
+    }
+}
+
+//create objects from the child classes
+$audi = new Audi("Audi", 50);
+$volvo = new Volvo("Volvo", 2);
+$citroen = new Citroen("Citroen", 45);
+
+//get the values
+echo $audi->intro();
+echo "<br>";
+echo $volvo->intro();
+echo "<br>";
+echo $citroen->intro();
+echo "<br>";
+
+//get the int values
+echo $audi->int();
+echo "<br>";
+echo $volvo->int();
+echo "<br>";
+echo $citroen->int();
+
+
+// class Fruit
+// {
+//     protected $name;
+//     protected $color;
+//     const LEAVING_MESSAGE = "Thank you for visiting our fruit stand!";
+
+//     public function __construct($name, $color)
+//     {
+//         $this->name = $name;
+//         $this->color = $color;
+//     }
+
+//     public function intro()
+//     {
+//         echo "The fruit sd sdsds is {$this->name} and the color is {$this->color}.";
+//     }
+// }
+
+// // Strawberry is inherited from Fruit
+// class Strawberry extends Fruit
+// {
+//     public $weight;
+
+//     public function __construct($name, $color, $weight)
+//     {
+//         $this->name = $name;
+//         $this->color = $color;
+//         $this->weight = $weight;
+//     }
+
+//     public function intro()
+//     {
+//         echo "The fruit is {$this->name}, the color is {$this->color}, and the weight is {$this->weight}";
+//     }
+
+//     public function addition($num1, $num2)
+//     {
+//         echo $num1 + $num2;
+//     }
+
+//     //byebye
+//     public function byebye()
+//     {
+//         echo self::LEAVING_MESSAGE;
+//     }
+// }
+// $strawberry = new Strawberry("Strawberry", "red", "30kg");
+// // $strawberry->addition(5, 10);
+// $strawberry->byebye();
